0f8f4b30cbbcc7876f85e73ef103c5348278c8a1
Tuhin Mukherjee <tuhinmukherjee74@gmail.com>
2022-01-16 19:03:50 +0530

    Routing lesson 1

---
+++ Learning routing/src/App.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import './App.css';
+import Nav from './Nav';
+import About from './About';
+import Shop from './Shop';
+import {BrowserRouter as Router, Switch, Route} from "react-router-dom";
+
+function App() {
+  return (
+    <Router>
+        <div className="App">
+            <Nav />
+            <Route path="/about" component={About} />
+            <Route path="/shop" component={Shop} />
+        </div>
+    </Router>
+  );
+}
+
+export default App;
---
+++ Learning routing/src/Nav.jsx
@@ -0,0 +1,17 @@
+import "./App.css";
+
+function Nav() {
+  return (
+    <div className="Nav">
+      <nav>
+          <h3>List of Pages</h3>
+          <ul className="nav-links">
+              <li>About page</li>
+              <li>Shop Page</li>
+          </ul>
+      </nav>
+    </div>
+  );
+}
+
+export default Nav;
